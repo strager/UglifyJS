f9907842bc73d9e8a2de48c1d3ec179b0e6fa92b
Matt Glazar <matt.glazar@gmail.com>
2011-07-01 11:04:49 -0700

    Failing test cases with name mangling and eval (due to v1.0.3)

---
+++ test/unit/compress/expected/mangle2.js
@@ -0,0 +1,1 @@
+(function(){function myEvaler(){eval("oogabooga")}var a={},foo=function(b,c,d){return c instanceof a}})()
---
+++ test/unit/compress/test/mangle2.js
@@ -0,0 +1,11 @@
+(function() {
+    var a = {};
+
+    function myEvaler() {
+        eval('ooga booga');
+    }
+
+    var foo = function foo(arg0, foo, arg2) {
+        return foo instanceof a;
+    };
+}());
